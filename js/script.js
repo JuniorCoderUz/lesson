58671d6334a721b2f2f90e7a3f0e767b826fdf71
SultonAliXan <juniorcoder.uz@gmail.com>
2020-08-02 02:35:43 +0500

    I've added new variable in script file

--- js/script.js
+++ js/script.js
@@ -1,15 +1,16 @@
 const numberOfFilms = +prompt("How many films have you seen?");
 
-const questionOne = prompt("One of films that you've seen recently","");
+const questionOne = prompt("One of films that you've seen recently", "");
 const questionTwo = prompt(`How do you rank the film - ${questionOne}`);
 
 const personalMovieDB = {
   count: numberOfFilms,
-  movies: {
-    questionOne: questionTwo
-  },
+  movies: {},
   actors: {},
   genres: [],
   private: false
 };
 
+personalMovieDB.movies["questionOne"] = questionTwo;
+
+console.log(personalMovieDB);
