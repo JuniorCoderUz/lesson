6f31c602e1fa3f206a7b833f373977ab913001bf
SultonAliXan <juniorcoder.uz@gmail.com>
2020-08-03 02:14:21 +0500

    lesson2.1

--- js/script.js
+++ js/script.js
@@ -1,49 +1,67 @@
-/*const numberOfFilms = +prompt("How many films have you seen?");
+// /*const numberOfFilms = +prompt("How many films have you seen?");
 
-const questionOne = prompt("One of films that you've seen recently", "");
-const questionTwo = prompt(`How do you rank the film - ${questionOne}`);
+// const questionOne = prompt("One of films that you've seen recently", "");
+// const questionTwo = prompt(`How do you rank the film - ${questionOne}`);
 
-const personalMovieDB = {
-  count: numberOfFilms,
-  movies: {},
-  actors: {},
-  genres: [],
-  private: false
-};
+// const personalMovieDB = {
+//   count: numberOfFilms,
+//   movies: {},
+//   actors: {},
+//   genres: [],
+//   private: false
+// };
 
-personalMovieDB.movies["questionOne"] = questionTwo;
+// personalMovieDB.movies["questionOne"] = questionTwo;
 
-console.log(personalMovieDB);
-*/
-/////////////////////////////////////////////////////////////////////////////
-/*
-Новая тема про операторы
-*/
-const num = 100;
+// console.log(personalMovieDB);
+// */
+// /////////////////////////////////////////////////////////////////////////////
+// /*
+// Новая тема про операторы
+// */
+// const num = 100;
 
-//Это оператор if-else для проверки кокогото конкретного ответа
-if (num == 30) {
-  console.log("Не правдо");
-} else if (num == 90) {
-  console.log('Нимношко')
-} else if (num == 100) {
-  console.log("Ты качок");
-} else {
-  console.log("Ты долб@#&");
-}
+// //Это оператор if-else для проверки кокогото конкретного ответа
+// if (num == 30) {
+//   console.log("Не правдо");
+// } else if (num == 90) {
+//   console.log('Нимношко');
+// } else if (num == 100) {
+//   console.log("Ты качок");
+// } else {
+//   console.log("Ты долб@#&");
+// }
 
-//Это тернарный оператор он сокрашает код
-(num == 100) ? console.log("You tell true") : console.log("Ты гонд0#");
+// //Это тернарный оператор он сокрашает код
+// // (num == 100) ? console.log("You tell true") : console.log("Ты гонд0#");
 
-//Это switch case для проверки до#уя придложений
-switch (num) {
-  case 30:
-    console.log("Ты ни#уя не угодал");
-    break;
-  case 100:
-    console.log("Ахты сосунок как ты угодаль");
-    break;
-  default:
-    console.log("Пшолвон!!!");
-    break;
+// //Это switch case для проверки до#уя придложений
+// switch (num) {
+//   case 30:
+//     console.log("Ты ни#уя не угодал");
+//     break;
+//   case 100:
+//     console.log("Ахты сосунок как ты угодаль");
+//     break;
+//   default:
+//     console.log("Пшолвон!!!");
+//     break;
+// }
+let test = 1;
+
+// while (test <= 8) {
+//   console.log(test);
+//   test++;
+// }
+
+// do {
+//   console.log(test);
+//   test++;
+// }
+// while (test <= 8);
+
+// let num = 1;
+
+for (let num = 1; num == 8; num++) {
+  console.log(num);
 }
