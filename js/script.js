34ba59ecbfc119444a3f3926daff5603092165c7
SultonAliXan <juniorcoder.uz@gmail.com>
2020-08-04 12:48:03 +0500

    lesson2.5

--- js/script.js
+++ js/script.js
@@ -13,45 +13,35 @@ let infoOne,
   questionTwo,
   num = 0;
 
-while (true) {
-  while (true) {
-    questionOne = prompt("One of films that you've seen recently", '');
-    if (questionOne != null && questionOne != '' && questionOne.length < 50) {
-      infoOne = "DO";
-      break;
-    }
-    else {
-      alert("Sorry Try Again");
-      continue;
-    }
+for (let i = 0; i <= 2; i++) {
+  questionOne = prompt("One of films that you've seen recently", '');
+  questionTwo = prompt(`How do you rank the film - ${questionOne}`);
+  if (questionOne != null && questionOne != '' && questionOne.length < 50 && questionTwo != null && questionTwo != '') {
+    personalMovieDB.movies["questionOne"] = questionTwo;
+    console.log("done")
+  }
+  else {
+    alert("Sorry Try Again");
+    i--;
   }
 
-  while (true) {
-    questionTwo = prompt(`How do you rank the film - ${questionOne}`);
-    if (questionTwo != null, questionTwo != '') {
-      infoOne += "NE";
-      break;
-    } else {
-      alert("Sorry Try Again");
-      continue;
-    }
 
-  }
-
-  if (personalMovieDB.count <= 10) {
-    alert("You watch too little TV");
-  } else if (personalMovieDB.count >= 20) {
-    alert("You're classic spectator");
-  } else if (personalMovieDB.count > 30) {
-    alert("You're a movie fan");
-  } else {
-    alert("Error 404 Try again");
-
-  }
-
-  continue;
 }
-personalMovieDB.movies["questionOne"] = questionTwo;
+
+if (personalMovieDB.count <= 10) {
+  alert("You watch too little TV");
+} else if (personalMovieDB.count >= 20) {
+  alert("You're classic spectator");
+} else if (personalMovieDB.count > 30) {
+  alert("You're a movie fan");
+} else {
+  alert("Error 404 Try again");
+}
+
+
+
+
 
 console.log(personalMovieDB);
 console.log(infoOne);
+
