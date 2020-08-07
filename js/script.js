651d83cb96e5c1cd5c5c73d381071f090b510726
SultonAliXan <juniorcoder.uz@gmail.com>
2020-08-07 17:17:03 +0500

    lesson2.6 function

--- js/script.js
+++ js/script.js
@@ -14,3 +14,16 @@ Two("Junior_Coder_Uz");
 
 one("SultonAlixan");
 
+let a = 3;
+let b = 2;
+
+const calc = (a, b) => a+b;
+calc(4 + 6);
+
+
+function calcs(x, y) {
+  console.log(x + y);
+
+}
+
+calcs(2, 1);
