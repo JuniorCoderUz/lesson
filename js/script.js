8e657f19e51c4e429b20fc35af5a0cb47adc0cff
SultonAliXan <juniorcoder.uz@gmail.com>
2020-08-17 00:30:52 +0500

    lesson2.11 pseudo objects and arrays ES6-ES9

--- js/script.js
+++ js/script.js
@@ -1,41 +1,80 @@
 "use strict";
+let a = 5,
+  b = a;
 
-
-function myFunction(main) {
-  let objsave = {};
-  let key;
-
-  for (key in main) {
-    objsave[key] = main[key];
-
-  }
-
-  return objsave;
-}
-
-const theNumbers = {
-  one: 1,
-  two: 2,
-  three: 3,
-  four: 4,
-  five: 5,
-  six: 6,
-  seven: 7,
-  eight: 8,
-  nine: 9,
-  ten: 10,
-  favorites: {
-    bla: 16,
-    kel: 9,
-    zero: 0
+b = b + 10;
+// console.log(a);
+// console.log(b);
+///////////////////////////////////////////////////////////////////
+const obj = {
+  a: 5,
+  b: 1,
+  c: {
+    x: 10,
+    y: 15,
   }
 };
+let copyOne = obj;
+copyOne.b = 10;
 
+// console.log(copyOne);
+// console.log(obj);
 
-let testNums;
-testNums.myFunction(theNumbers);
+function copy(mainObj) {
+  let objCopy = {};
 
-testNums.six = 16;
-testNums.favorites.kel = 5;
-console.log(theNumbers);
-console.log(testNums);
+  for (let key in mainObj) {
+    objCopy[key] = mainObj[key];
+  }
+  return objCopy;
+}
+
+copyOne = copy(obj);
+copyOne.a = 2;
+copyOne.c.x = 16;
+
+// console.log(obj);
+// console.log(copyOne);
+
+const newNumbers = {
+  d: 2,
+  i: 9,
+};
+
+let allObj = Object.assign({}, copyOne, newNumbers);//method №1
+// Object.assign(allObj, newNumbers, copyOne);//method №2
+allObj.a = 20;
+// console.log(copyOne);
+// console.log(allObj);
+////////////////////////////////////////////////////////////////////////
+const oldArray = ['a', 'b', 'c'];
+const newArray = oldArray.slice();//method №1
+// const newArray = {...oldArray};//method №2
+newArray[3] = 'd';
+// console.log(oldArray);
+// console.log(newArray);
+///////////////////////////////////////////////////////
+
+const socialNetworks = ['facebook', 'telegram', 'ok', 'vk', 'whatsapp'],
+  video = ['youtube', 'uzdigitaltv', 'vimeo', 'rutube'],
+  internet = [...socialNetworks, ...video, 'deepweb', 'darkweb'];
+
+// console.log(internet);
+//////////////////////////////////////////////////////////////////////////
+function log(a, b, c) {
+  console.log(a);
+  console.log(b);
+  console.log(c);
+}
+
+// const nums = [1, 2, 3];
+// log(...nums);
+///////////////////////////////////////////////////////////////////////////
+const testObj = {
+  one: 'one',
+  two: 'two',
+};
+
+const q = { ...testObj };
+
+// console.log(q);
