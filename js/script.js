5eeec182e1c23efcf3672ed32dd5ee3ff7fac5b4
SultonAliXan <juniorcoder.uz@gmail.com>
2020-08-19 01:24:30 +0500

    lesson2.13 execirses 4th

--- js/script.js
+++ js/script.js
@@ -1,36 +1,86 @@
 "use strict";
-const str = "Some";
-let strObj = new String(str);
+let numberOfFilms;
 
-// console.log(typeof (str));
-// console.log(typeof (strObj));
 
-// console.dir([1, 2, 3]);
-////////////////////////////////////////////////////////////////////////////
-const soldier = {
-  health: 400,
-  armor: 100,
-  myprint: () => console.log("Hello World"),
+let infoOne,
+  questionOne,
+  questionTwo,
+  num = 0,
+  questionGenres;
+
+
+const personalMovieDB = {
+  count: numberOfFilms,
+  movies: {},
+  actors: {},
+  genres: [],
+  private: false,
+  start: () => {
+    personalMovieDB.count = +prompt("How many films have you seen?", 1);
+    while (personalMovieDB.count == '' || personalMovieDB.count == null || isNaN(personalMovieDB.count)) {
+      personalMovieDB.count = +prompt("How many films have you seen?", 1);
+    }
+  },
+  rememberMyFilm: () => {
+    do {
+      questionOne = prompt("One of films that you've seen recently", '');
+      questionTwo = prompt(`How do you rank the film - ${questionOne}`);
+      if (questionOne != null && questionOne != '' && questionOne.length < 50 && questionTwo != null && questionTwo != '') {
+        personalMovieDB.movies["questionOne"] = questionTwo;
+        console.log("done");
+        console.log("DONE");
+        num++;
+      } else {
+        alert("Sorry Try again");
+        continue;
+      }
+    } while (num < 3);
+  },
+  detectPersonalLevel: () => {
+    if (personalMovieDB.count <= 10) {
+      alert("You watching too few tv");
+    } else if (personalMovieDB.count >= 10 && personalMovieDB.count < 30) {
+      alert("You're classic spectator");
+    } else if (personalMovieDB.count >= 30) {
+      alert("You're watch tv a fun");
+    } else {
+      alert("Sorry try again");
+    }
+
+  },
+  showMyDB: (hidden) => {
+    if (!hidden) {
+      console.log(personalMovieDB);
+    }
+  },
+  toggleVisibleMyDB: () => {
+    if (personalMovieDB.private) {
+      personalMovieDB.private = false;
+    } else {
+      personalMovieDB.private = true;
+    }
+  },
+  writeYourGenres: () => {
+    for (let i = 1; i < 2; i++) {
+      //   questionGenres = prompt(`You are favorite genre at number ${i}`);
+      //   if (questionGenres !== null && questionGenres !== '') {
+      //     personalMovieDB.genres[i - 1] = questionGenres;
+      //   }
+      //   else {
+      //     i--;
+      //   }
+      questionGenres = prompt(`Please enter you're favorite genres after commas`).toLowerCase;
+
+      if (questionGenres !== null && questionGenres !== '') {
+        personalMovieDB.genres = questionGenres.split(', ');
+        personalMovieDB.genres.sort();
+      }
+      else {
+        i--;
+      }
+    }
+    personalMovieDB.genres.forEach(function (value, indexNum) {
+      console.log(`Favorite genre ${indexNum + 1} - ${value}`);
+    });
+  }
 };
-// soldier.myprint();
-
-
-
-// const ronald = {
-//   health: 150,
-// };
-// ronald.__proto__ = soldier; //old method for proto №1
-// console.log(ronald);
-// console.log(ronald.myprint);
-///////////////////////////////////////////////////////////////////////////////
-
-const benjamin = {
-  health: 50,
-  profession: ['hacker', 'programmer'],
-};
-// Object.setPrototypeOf(benjamin, soldier); //method for proto №2
-// console.log(benjamin.armor);
-
-
-// const ben = Object.create(soldier); //new method for proto №3
-// ben.myprint();
