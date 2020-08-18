4054130408d39647bb92bacfa75ae85de93f2e82
SultonAliXan <juniorcoder.uz@gmail.com>
2020-08-18 16:26:25 +0500

    lesson2.12 basics of objects oriented programming, prototype-orientied inheritance

--- js/script.js
+++ js/script.js
@@ -1,80 +1,36 @@
 "use strict";
-let a = 5,
-  b = a;
+const str = "Some";
+let strObj = new String(str);
 
-b = b + 10;
-// console.log(a);
-// console.log(b);
-///////////////////////////////////////////////////////////////////
-const obj = {
-  a: 5,
-  b: 1,
-  c: {
-    x: 10,
-    y: 15,
-  }
+// console.log(typeof (str));
+// console.log(typeof (strObj));
+
+// console.dir([1, 2, 3]);
+////////////////////////////////////////////////////////////////////////////
+const soldier = {
+  health: 400,
+  armor: 100,
+  myprint: () => console.log("Hello World"),
 };
-let copyOne = obj;
-copyOne.b = 10;
+// soldier.myprint();
 
-// console.log(copyOne);
-// console.log(obj);
 
-function copy(mainObj) {
-  let objCopy = {};
 
-  for (let key in mainObj) {
-    objCopy[key] = mainObj[key];
-  }
-  return objCopy;
-}
+// const ronald = {
+//   health: 150,
+// };
+// ronald.__proto__ = soldier; //old method for proto №1
+// console.log(ronald);
+// console.log(ronald.myprint);
+///////////////////////////////////////////////////////////////////////////////
 
-copyOne = copy(obj);
-copyOne.a = 2;
-copyOne.c.x = 16;
-
-// console.log(obj);
-// console.log(copyOne);
-
-const newNumbers = {
-  d: 2,
-  i: 9,
+const benjamin = {
+  health: 50,
+  profession: ['hacker', 'programmer'],
 };
+// Object.setPrototypeOf(benjamin, soldier); //method for proto №2
+// console.log(benjamin.armor);
 
-let allObj = Object.assign({}, copyOne, newNumbers);//method №1
-// Object.assign(allObj, newNumbers, copyOne);//method №2
-allObj.a = 20;
-// console.log(copyOne);
-// console.log(allObj);
-////////////////////////////////////////////////////////////////////////
-const oldArray = ['a', 'b', 'c'];
-const newArray = oldArray.slice();//method №1
-// const newArray = {...oldArray};//method №2
-newArray[3] = 'd';
-// console.log(oldArray);
-// console.log(newArray);
-///////////////////////////////////////////////////////
 
-const socialNetworks = ['facebook', 'telegram', 'ok', 'vk', 'whatsapp'],
-  video = ['youtube', 'uzdigitaltv', 'vimeo', 'rutube'],
-  internet = [...socialNetworks, ...video, 'deepweb', 'darkweb'];
-
-// console.log(internet);
-//////////////////////////////////////////////////////////////////////////
-function log(a, b, c) {
-  console.log(a);
-  console.log(b);
-  console.log(c);
-}
-
-// const nums = [1, 2, 3];
-// log(...nums);
-///////////////////////////////////////////////////////////////////////////
-const testObj = {
-  one: 'one',
-  two: 'two',
-};
-
-const q = { ...testObj };
-
-// console.log(q);
+// const ben = Object.create(soldier); //new method for proto №3
+// ben.myprint();
