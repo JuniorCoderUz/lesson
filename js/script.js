77a50498c6a3c0ed046e8312a5f3e0ec81c36554
SultonAliXan <juniorcoder.uz@gmail.com>
2020-08-24 01:56:15 +0500

    test1.0

--- js/script.js
+++ js/script.js
@@ -1,26 +1,18 @@
 "use strict";
-//Get Id element in html document
-let box = document.getElementById('box');
-console.log(box);
-/////////////////////////////////////////////////////////////
-//Get class elements in html document
-// 1) method getElementsByClassName
-let circle = document.getElementsByClassName('circle');
-console.log(circle);
-/////////////////////////////////////////////////////////////
-//Get class elements in html document
-// 2) method querySelectorAll
-let hearts = document.querySelectorAll('.heart');
-console.log(hearts);
-hearts.forEach(items => {
-  console.log(items);
-});
-/////////////////////////////////////////////////////////////
-//Get class element in html document 
-//3) method querySelector 
-let oneHeart = document.querySelector('.heart');
-console.log(oneHeart);
-/////////////////////////////////////////////////////////////
-//Get jtml tag in html document  
-let tag = document.getElementsByTagName('button');
-console.log(tag);
+// let sharp = "#";
+// let one = "#";
+// for (let i = 0; i < 7; i++) {
+//   console.log(sharp);
+//   sharp = sharp+one;
+// }
+
+for (let a = 1; a < 100; a++) {
+  if (a % 2 == 0) {
+    console.log(a);
+  } else if (a % 2 == 1) {
+    console.log("Bizz");
+  }
+  else {
+    console.log("Fizz");
+  }
+}
