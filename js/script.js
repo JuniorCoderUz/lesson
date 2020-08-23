287730abaf0d630e6da6b177e34644a0b7744b03
SultonAliXan <juniorcoder.uz@gmail.com>
2020-08-23 17:41:59 +0500

    lesson2.16 get id, selector and class in html document DOM

--- js/script.js
+++ js/script.js
@@ -1,32 +1,26 @@
 "use strict";
-// let x = 5;
-// alert(x);
-/////////////////////////////////////////////
-// let testBool = [] + false + null + true;
-// console.log(Boolean(testBool));
-/////////////////////////////////////////////
-// let y = 1; let x = y = 2; alert(x); 
-/////////////////////////////////////////////
-// let testNum = [] + 1 + 2;
-// console.log(testNum.length);
-/////////////////////////////////////////////
-// console.log("25"[1]);
-/////////////////////////////////////////////
-// console.log(Boolean(2 && 1 && null && 0 && undefined));
-/////////////////////////////////////////////
-// let a = true,
-//   b = false;
-// console.log(Boolean(!!(a && b)));
-/////////////////////////////////////////////
-// alert(Boolean(null || 20 && 10 || 4));
-/////////////////////////////////////////////
-// let a = [1, 2, 3],
-//   b = [1, 2, 3];
-// console.log(a === b);
-/////////////////////////////////////////////
-// alert(+"Infinity");
-/////////////////////////////////////////////
-// console.log(Boolean("Ёжик" > "яблоко"));
-/////////////////////////////////////////////
-console.log(0 || "" || 2 || undefined || true || false);
-
+//Get Id element in html document
+let box = document.getElementById('box');
+console.log(box);
+/////////////////////////////////////////////////////////////
+//Get class elements in html document
+// 1) method getElementsByClassName
+let circle = document.getElementsByClassName('circle');
+console.log(circle);
+/////////////////////////////////////////////////////////////
+//Get class elements in html document
+// 2) method querySelectorAll
+let hearts = document.querySelectorAll('.heart');
+console.log(hearts);
+hearts.forEach(items => {
+  console.log(items);
+});
+/////////////////////////////////////////////////////////////
+//Get class element in html document 
+//3) method querySelector 
+let oneHeart = document.querySelector('.heart');
+console.log(oneHeart);
+/////////////////////////////////////////////////////////////
+//Get jtml tag in html document  
+let tag = document.getElementsByTagName('button');
+console.log(tag);
